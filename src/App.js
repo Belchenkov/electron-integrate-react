12b128b9d64d629392f80206ef49aacc37ec62bb
Aleksey Belchenkov <u608110@gmail.com>
2020-05-03 18:10:53 +0300

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { Navbar, Button, Alignment } from '@blueprintjs/core';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
 
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Files() {
+  return <h2>Files</h2>;
+}
+
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar className="bp3-dark">
         <Navbar.Group align={Alignment.LEFT}>
           <Navbar.Heading>Blueprint</Navbar.Heading>
           <Navbar.Divider />
-          <Button className="bp3-minimal" icon="home" text="Home" />
-          <Button className="bp3-minimal" icon="document" text="Files" />
+          <Button className="bp3-minimal">
+            <Link
+                to="/"
+                className="bp3-button bp3-minimal bp3-icon-home"
+            >Home</Link>
+          </Button>
+          <Button className="bp3-minimal">
+            <Link
+                to="/files/"
+                className="bp3-button bp3-minimal bp3-icon-document"
+            >Files</Link>
+          </Button>
         </Navbar.Group>
       </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          <Route path="/" exact component={Index} />
+          <Route path="/files/" component={Files} />
         <h1>React Electron</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -30,7 +51,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Router>
   );
 }
 
